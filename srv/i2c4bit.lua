b379be4a63b9ae60c41d74631fdb29e777a7dd42
Matsievskiy S.V <matsievskiysv@gmail.com>
2019-12-27 21:23:13 +0300

    change i2c bitmap

--- srv/i2c4bit.lua
+++ srv/i2c4bit.lua
@@ -1,23 +1,33 @@
 local I2C4bit = {}
 
--- command bits
-local LCD_RS = 0x1
-local LCD_RW = 0x2
-local LCD_EN = 0x4
-local LCD_BACKLIGHT = 0x8
+-- I2C GPIO expander bitmap
+local LCD_RS = 0
+local LCD_RW = 1
+local LCD_EN = 2
+local LCD_BACKLIGHT = 3
+local LCD_D4 = 4
+local LCD_D5 = 5
+local LCD_D6 = 6
+local LCD_D7 = 7
 
 local function send4bitI2C(value, cmd, bus_args, backlight)
-   local hi = bit.band(value, 0xf0)
-   local lo = bit.lshift(bit.band(value, 0xf), 4)
+   local hi = bit.bor(bit.lshift(bit.band(bit.rshift(value, 4), 1), LCD_D4),
+		      bit.lshift(bit.band(bit.rshift(value, 5), 1), LCD_D5),
+		      bit.lshift(bit.band(bit.rshift(value, 6), 1), LCD_D6),
+		      bit.lshift(bit.band(bit.rshift(value, 7), 1), LCD_D7))
+   local lo = bit.bor(bit.lshift(bit.band(bit.rshift(value, 0), 1), LCD_D4),
+		      bit.lshift(bit.band(bit.rshift(value, 1), 1), LCD_D5),
+		      bit.lshift(bit.band(bit.rshift(value, 2), 1), LCD_D6),
+		      bit.lshift(bit.band(bit.rshift(value, 3), 1), LCD_D7))
    if backlight then
-      cmd = bit.bor(cmd, LCD_BACKLIGHT)
+      cmd = bit.bor(cmd, bit.lshift(1, LCD_BACKLIGHT))
    end
    i2c.start(bus_args.id)
    i2c.address(bus_args.id, bus_args.address, i2c.TRANSMITTER)
-   i2c.write(bus_args.id, bit.bor(hi, bit.bor(cmd, LCD_EN)))
-   i2c.write(bus_args.id, bit.bor(hi, bit.band(cmd, bit.bnot(LCD_EN))))
-   i2c.write(bus_args.id, bit.bor(lo, bit.bor(cmd, LCD_EN)))
-   i2c.write(bus_args.id, bit.bor(lo, bit.band(cmd, bit.bnot(LCD_EN))))
+   i2c.write(bus_args.id, bit.bor(hi, cmd, bit.lshift(1, LCD_EN)))
+   i2c.write(bus_args.id, bit.bor(hi, bit.band(cmd, bit.bnot(bit.lshift(1, LCD_EN)))))
+   i2c.write(bus_args.id, bit.bor(lo, cmd, bit.lshift(1, LCD_EN)))
+   i2c.write(bus_args.id, bit.bor(lo, bit.band(cmd, bit.bnot(bit.lshift(1, LCD_EN)))))
    i2c.stop(bus_args.id)
 end
 
@@ -26,7 +36,7 @@ I2C4bit.command = function(self, value)
 end
 
 I2C4bit.write = function(self, value)
-   send4bitI2C(value, LCD_RS, self._bus_args, self._backlight)
+   send4bitI2C(value, bit.lshift(1, LCD_RS), self._bus_args, self._backlight)
 end
 
 I2C4bit.backlight = function(self, on)
